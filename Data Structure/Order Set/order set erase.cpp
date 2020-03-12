abc9c0ba0741d68b72b1b15ac72f98dad4c9af7e
Prangan Majumder <33204840+pranganmajumder@users.noreply.github.com>
2020-03-12 09:49:53 -0700

    Add files via upload

---
+++ Data Structure/Order Set/order set erase.cpp
@@ -0,0 +1,99 @@
+
+/**OOO**/
+#include<bits/stdc++.h>
+typedef long long int ll;
+typedef unsigned long long ull;
+using namespace std;
+
+
+#define mems(arr,VAL)           memset(arr,VAL,sizeof(arr))
+#define pii                     pair<int,int>
+#define mk                      make_pair
+#define pb                      push_back
+#define ff                      first
+#define ss                      second
+#define PI                      acos(-1.0)
+
+#define pf                      printf
+#define sc                      scanf
+//#define Mod                     1000000007
+#define hiii                    1e9//1000000000
+#define looo                   -1e9//-1000000000
+
+// Ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(1)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+
+struct info
+{
+
+    int w,indx;
+    info(int a,int b){
+        w=a;
+        indx=b;
+    }
+    bool operator<(const info& p)const{
+        if(w==p.w){
+            return indx<p.indx;
+        }
+        else{
+            return w>p.w;
+        }
+    }
+};
+
+
+int main()
+{
+    //ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    //clock_t startTime = clock() ;
+    //freopen("A1.in","r",stdin);
+    //freopen("A1.out","w",stdout);
+
+    //struct_type arr[101];
+    //sort(arr,arr+n,compare);
+
+    orderset<info>s;
+    orderset<info>::iterator it;
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s.insert(info(x,i));
+    }
+
+    for(it=s.begin();it!=s.end();it++){
+        info x = *it ;
+        //cout<<x.first<<" "<<x.second<<endl;
+        cout<<x.w<<"       "<<x.indx<<endl;
+    }
+    //cout<<(*s.find_by_order(info(2)))<<endl;
+   // cout<<(s.order_of_key(info(11,2)))<<endl;
+
+
+    //s.erase(info(value,indx))//value o index bosay dao
+
+/**
+5
+50 60 10 20 30
+
+
+11
+
+11 11 5 5 5 11 11 5 5 11 11
+
+**/
+
+//cout << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
+return 0;
+}
+
+
+
