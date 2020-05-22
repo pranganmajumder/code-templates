482f74cb41fed8a1102ccbd98249842f983338cb
pranganmajumder <prangan.m@gmail.com>
2020-05-23 02:15:52 +0600

    Added a function addSum

--- Data Structure/Linked List/raw.cpp
+++ Data Structure/Linked List/raw.cpp
@@ -2,7 +2,13 @@
 using namespace std ;
 
 
+int addSum(int a , int b){
+    return a+b ;
+}
+
 int main()
 {
     cout << "This is a demo raw file for Linked List Push" << endl ;
+    int res = addSum(4,5) ;
+    cout << "result is    " << res << endl ;
 }
