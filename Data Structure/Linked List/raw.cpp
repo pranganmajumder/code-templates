4bac983420f48b1fe5e4d5d95fd843b2fad6e1c9
pranganmajumder <prangan.m@gmail.com>
2020-05-24 01:30:45 +0600

    remove ret and add again method

--- Data Structure/Linked List/raw.cpp
+++ Data Structure/Linked List/raw.cpp
@@ -6,10 +6,10 @@ int addSum(int a , int b){
     return a+b ;
 }
 
-void ret(int n = 3){
+void again(int n = 3){
     if(n == 20)return ;
     cout << "n  " << n << endl ;
-    ret(n+1) ;
+    again(n+1) ;
 }
 
 int main()
@@ -17,5 +17,5 @@ int main()
     cout << "This is a demo raw file for Linked List Push" << endl ;
     int res = addSum(4,5) ;
     cout << "result is    " << res << endl ;
-    ret() ;
+    again() ;
 }
