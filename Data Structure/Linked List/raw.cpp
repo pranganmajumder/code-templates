7d9412ed11b86d48e498a6e8532168f61504d516
pranganmajumder <prangan.m@gmail.com>
2020-05-24 01:17:51 +0600

    A new method prinMyname

--- Data Structure/Linked List/raw.cpp
+++ Data Structure/Linked List/raw.cpp
@@ -6,9 +6,15 @@ int addSum(int a , int b){
     return a+b ;
 }
 
+
+void prinMyName(){
+    cout << "prangan Majumder " << endl ;
+}
+
 int main()
 {
     cout << "This is a demo raw file for Linked List Push" << endl ;
     int res = addSum(4,5) ;
     cout << "result is    " << res << endl ;
+    prinMyName() ;
 }
