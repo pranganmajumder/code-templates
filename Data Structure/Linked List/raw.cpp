e121bcf5d36daa34bb47df0822dfa6a94abdca41
pranganmajumder <prangan.m@gmail.com>
2020-05-24 01:14:06 +0600

    added a new recursive function

--- Data Structure/Linked List/raw.cpp
+++ Data Structure/Linked List/raw.cpp
@@ -6,9 +6,16 @@ int addSum(int a , int b){
     return a+b ;
 }
 
+void ret(int n = 3){
+    if(n == 20)return ;
+    cout << "n  " << n << endl ;
+    ret(n+1) ;
+}
+
 int main()
 {
     cout << "This is a demo raw file for Linked List Push" << endl ;
     int res = addSum(4,5) ;
     cout << "result is    " << res << endl ;
+    ret() ;
 }
